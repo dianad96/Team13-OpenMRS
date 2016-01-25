5c6b461e339bba7f510bc9b75b8ee38279a34970
Diana Darie <zcabdad@ucl.ac.uk>
2016-01-25 15:28:55 +0000

    Changes

---
+++ team13AndroidClientmaster/src/main/java/org/openmrs/mobile/activities/InputFood.java
@@ -0,0 +1,15 @@
+package org.openmrs.mobile.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.openmrs.mobile.R;
+
+public class InputFood extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_food);
+    }
+}
