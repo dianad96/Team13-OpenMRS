88f7c15c58be3e76ce5878f437056b6e385a6cee
chevyng <seasoneva@gmail.com>
2016-03-11 04:43:27 +0000

    SyncFitbit and Graph update
    1) SyncFitBitService minor changes to how data for distance is
    retrieved.
    2) Graph page now shows BMI and Exercise Minutes(on actual day only)
    
    TODO
    1) Get Heart Rate data from OpenMRS server
    2) Get Target Heart Rate data from OpenMRS server

--- team13AndroidClientmaster/build/generated/source/r/debug/android/support/v7/gridlayout/R.java
+++ team13AndroidClientmaster/build/generated/source/r/debug/android/support/v7/gridlayout/R.java
@@ -11,11 +11,13 @@ public final class R {
 		public static final int alignmentMode = 0x7f01004b;
 		public static final int columnCount = 0x7f010049;
 		public static final int columnOrderPreserved = 0x7f01004d;
-		public static final int layout_column = 0x7f010050;
-		public static final int layout_columnSpan = 0x7f010051;
-		public static final int layout_gravity = 0x7f010052;
+		public static final int layout_column = 0x7f010051;
+		public static final int layout_columnSpan = 0x7f010052;
+		public static final int layout_columnWeight = 0x7f010053;
+		public static final int layout_gravity = 0x7f010054;
 		public static final int layout_row = 0x7f01004e;
 		public static final int layout_rowSpan = 0x7f01004f;
+		public static final int layout_rowWeight = 0x7f010050;
 		public static final int orientation = 0x7f010047;
 		public static final int rowCount = 0x7f010048;
 		public static final int rowOrderPreserved = 0x7f01004c;
@@ -46,7 +48,7 @@ public final class R {
 	}
 	public static final class styleable {
 		public static final int[] GridLayout = { 0x7f010047, 0x7f010048, 0x7f010049, 0x7f01004a, 0x7f01004b, 0x7f01004c, 0x7f01004d };
-		public static final int[] GridLayout_Layout = { 0x010100f4, 0x010100f5, 0x010100f6, 0x010100f7, 0x010100f8, 0x010100f9, 0x010100fa, 0x7f01004e, 0x7f01004f, 0x7f010050, 0x7f010051, 0x7f010052 };
+		public static final int[] GridLayout_Layout = { 0x010100f4, 0x010100f5, 0x010100f6, 0x010100f7, 0x010100f8, 0x010100f9, 0x010100fa, 0x7f01004e, 0x7f01004f, 0x7f010050, 0x7f010051, 0x7f010052, 0x7f010053, 0x7f010054 };
 		public static final int GridLayout_Layout_android_layout_height = 1;
 		public static final int GridLayout_Layout_android_layout_margin = 2;
 		public static final int GridLayout_Layout_android_layout_marginBottom = 6;
@@ -54,11 +56,13 @@ public final class R {
 		public static final int GridLayout_Layout_android_layout_marginRight = 5;
 		public static final int GridLayout_Layout_android_layout_marginTop = 4;
 		public static final int GridLayout_Layout_android_layout_width = 0;
-		public static final int GridLayout_Layout_layout_column = 9;
-		public static final int GridLayout_Layout_layout_columnSpan = 10;
-		public static final int GridLayout_Layout_layout_gravity = 11;
+		public static final int GridLayout_Layout_layout_column = 10;
+		public static final int GridLayout_Layout_layout_columnSpan = 11;
+		public static final int GridLayout_Layout_layout_columnWeight = 12;
+		public static final int GridLayout_Layout_layout_gravity = 13;
 		public static final int GridLayout_Layout_layout_row = 7;
 		public static final int GridLayout_Layout_layout_rowSpan = 8;
+		public static final int GridLayout_Layout_layout_rowWeight = 9;
 		public static final int GridLayout_alignmentMode = 4;
 		public static final int GridLayout_columnCount = 2;
 		public static final int GridLayout_columnOrderPreserved = 6;
